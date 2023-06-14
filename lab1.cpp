7988f604502f69f596fe1c71a30286830bacc6c5
lriv42 <129525070+lriv42@users.noreply.github.com>
2023-06-14 15:49:26 -0500

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,20 @@
+/*
+	Title:  	lab1.cpp
+	Author:	Liam Rivers
+	Date:		5/23/2023
+	Purpose:	Convert Celsius to Fahrenheit
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double fahrenheit, celsius;
+	cout << "\n\nWhat is the temperature in Celsius?  ";
+	cin >> celsius;
+	fahrenheit = celsius * (9.0/5.0) + 32;
+	cout << "\nRESULT:  " << celsius << " degrees Celsius is ";
+      cout << fahrenheit << " degrees Fahrenheit.\n\n";
+	return 0;
+}
